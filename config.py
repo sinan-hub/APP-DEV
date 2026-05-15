9e38bd3f15f8a7a27215ad4d455abe87d3e520ee
sinan-hub <sinanameersh@gmail.com>
2026-05-15 11:42:33 +0530

    Added the dockerfile

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///placement_portal.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'placement_portal.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
